e50489e02244281b4373b8c687be6f5d5c24e203
李畅 <246921039@qq.com>
2025-10-15 15:02:01 +0800

    Add files via upload

---
+++ sum minus chang1.c
@@ -0,0 +1,7 @@
+# include <stdio.h>
+int main() 
+{ int a,b,sum,minus; 
+a=123;b=456; 
+printf("a+b=%d\na-b=%d\n",sum,minus); 
+return 0;
+}//a=b=0 a-b=23
---
+++ tiaoshichengxu.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    x=5;
+    y=8;
+    printf("\n%d,%d,%d\n",x,(x+5)*2,y);
+    return 0;
+ }
+ //错误为未定义y，和x和y的赋值应用；隔开
